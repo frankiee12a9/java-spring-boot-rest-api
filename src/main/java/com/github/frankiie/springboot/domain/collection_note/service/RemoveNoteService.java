2b8d66da5a40e1c2a5cbf766ce440086d4269a27
TKNguyen12a9 <cudayanh@gmail.com>
2023-03-29 02:04:39 +0900

    Note belongings

---
+++ src/main/java/com/github/frankiie/springboot/domain/collection_note/service/RemoveNoteService.java
@@ -0,0 +1,16 @@
+package com.github.frankiie.springboot.domain.collection_note.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class RemoveNoteService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RemoveNoteService.class);
+  
+    // todo
+}
