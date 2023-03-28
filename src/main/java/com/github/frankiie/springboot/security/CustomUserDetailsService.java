616e8606546074334653deda8e4d3060c3352729
TKNguyen12a9 <cudayanh@gmail.com>
2023-03-28 23:53:40 +0900

    remvove to reup

--- src/main/java/com/github/frankiie/springboot/security/CustomUserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.github.frankiie.springboot.security;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface CustomUserDetailsService {
-
-    UserDetails loadUserByUsername(String name);
-    UserDetails loadUserById(Long id);
-}
